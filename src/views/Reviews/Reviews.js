8c6eb7197ceef1f01ee682d6c2d5acb4c775499e
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-02 17:54:55 +0300

    adds component Reviews and its styles

--- src/views/Reviews/Reviews.js
+++ src/views/Reviews/Reviews.js
@@ -1,3 +1,26 @@
-export default function Reviews() {
-  return;
-}
+import React from 'react';
+import PropTypes from 'prop-types';
+// import s from './Reviews.module.css';
+
+const Reviews = ({ reviewsInfo }) => {
+  return (
+    <>
+      {reviewsInfo > 0 ? (
+        reviewsInfo.map(review => (
+          <li key={review.id}>
+            <h2>{review.author}</h2>
+            <p>{review.content}</p>
+          </li>
+        ))
+      ) : (
+        <p>We don't have any reviews for this movie...</p>
+      )}
+    </>
+  );
+};
+
+Reviews.propTypes = {
+  reviewsInfo: PropTypes.arrayOf(PropTypes.shape),
+};
+
+export default Reviews;
