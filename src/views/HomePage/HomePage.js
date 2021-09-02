b92242064c7dc55d159f55115974c388e318d5ab
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-02 17:52:36 +0300

    adds component HomePage and its styles

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,3 +1,43 @@
-export default function HomePage() {
-  return;
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+import * as movieAPI from '../../services/movie-api';
+import s from './HomePage.module.css';
+
+export default function Home() {
+  const location = useLocation();
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    movieAPI
+      .fetchTrendingMovies()
+      .then(response => setTrendingMovies(response.results));
+  }, []);
+
+  return (
+    <>
+      <h1 className={s.HomeTitle}>Trending today</h1>
+
+      {trendingMovies && (
+        <ul>
+          {trendingMovies.map(movie => {
+            return (
+              <li key={movie.id} className={s.HomeListItem}>
+                <Link
+                  to={{
+                    pathname: `/movies/${movie.id}`,
+                    state: { from: location },
+                  }}
+                  className={s.HomeListLink}
+                >
+                  {movie.title}
+                  {movie.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
 }
