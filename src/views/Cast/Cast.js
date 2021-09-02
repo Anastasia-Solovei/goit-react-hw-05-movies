7a3b8682ed1bb35a2259110010c858bd9d3b21b5
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-02 17:51:20 +0300

    adds component Cast and its styles

--- src/views/Cast/Cast.js
+++ src/views/Cast/Cast.js
@@ -1,3 +1,45 @@
-export default function Cast() {
-  return;
-}
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BASE_IMG_URL } from '../../services/movie-api';
+import defaultImg from '../../images/no_image_found.jpg';
+import s from './Cast.module.css';
+
+const Cast = ({ castInfo }) => {
+  return (
+    <>
+      {castInfo && (
+        <ul>
+          {castInfo.map(actor => {
+            return (
+              <li key={actor.id}>
+                <div className={s.CastProfileImgContainer}>
+                  <img
+                    src={
+                      actor.profile_path
+                        ? `${BASE_IMG_URL}${actor.profile_path}`
+                        : defaultImg
+                    }
+                    alt={actor.original_name}
+                    className={s.CastProfileImg}
+                  ></img>
+                </div>
+                <h2>{actor.original_name}</h2>
+                <p>Character: {actor.character}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
+};
+
+Cast.defaultProps = {
+  imgURL: '../../images/no_poster_found.png',
+};
+
+Cast.propTypes = {
+  castInfo: PropTypes.arrayOf(PropTypes.shape),
+};
+
+export default Cast;
