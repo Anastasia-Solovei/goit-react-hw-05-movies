9b8c30cd3aff75d26f31588632443cd6002f8f19
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-02 17:54:17 +0300

    adds component MoviesPage and its styles

--- src/views/MoviesPage/MoviesPage.js
+++ src/views/MoviesPage/MoviesPage.js
@@ -1,3 +1,59 @@
+import { useState, useEffect } from 'react';
+import { Link, useRouteMatch, useHistory, useLocation } from 'react-router-dom';
+
+import * as movieAPI from '../../services/movie-api';
+import SearchBar from '../SearchBar';
+import s from './MoviesPage.module.css';
+
 export default function MoviesPage() {
-  return;
+  const { url } = useRouteMatch();
+  const history = useHistory();
+  const location = useLocation();
+
+  const [movies, setMovies] = useState([]);
+  const searchQuery = new URLSearchParams(location.search).get('query') ?? '';
+
+  const onSubmitHandler = input => {
+    history.push({
+      ...location,
+      search: `query=${input}`,
+    });
+  };
+
+  useEffect(() => {
+    if (searchQuery === '') {
+      return;
+    }
+
+    movieAPI.fetchSearchMovies(searchQuery).then(({ results }) => {
+      setMovies(results);
+    });
+  }, [searchQuery]);
+
+  return (
+    <div className={s.moviesPage}>
+      <SearchBar onSubmit={onSubmitHandler} />
+
+      {movies && (
+        <ul>
+          {movies.map(movie => {
+            return (
+              <li key={movie.id} className={s.MoviesListItem}>
+                <Link
+                  to={{
+                    pathname: `${url}/${movie.id}`,
+                    state: { from: location },
+                  }}
+                  className={s.MoviesListLink}
+                >
+                  {movie.title}
+                  {movie.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
