a7c744b950189d107268ae03798589b9ff58b863
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-02 17:50:01 +0300

    adds component AppBar and its styles

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation';
+
+import s from './AppBar.module.css';
+
 export default function AppBar() {
-  return;
+  return (
+    <header className={s.AppBar}>
+      <Navigation />
+    </header>
+  );
 }
